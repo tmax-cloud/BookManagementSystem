ae575201a45e209c9f2f5d506d943921ef632380
taejune <taejune_ahn@tmax.co.kr>
2021-12-03 09:26:49 +0900

    [style] Remove unnecessary imports

--- core/src/main/java/org/tmaxcloud/sample/msa/book/core/BookController.java
+++ core/src/main/java/org/tmaxcloud/sample/msa/book/core/BookController.java
@@ -4,10 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
